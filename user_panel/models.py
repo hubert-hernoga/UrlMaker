f437f59476e5d192ec55a09e085fedb4a2e0a0bc
wezymord <hubert.hernoga@wp.pl>
2018-05-09 13:51:08 +0200

    First migration

--- user_panel/models.py
+++ user_panel/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', default=None)
+    birth_date = models.DateField(default=None)
+    list_groups = models.CharField(max_length=64, null=True)
+
+class UserGroups(models.Model):
+    name = models.CharField(max_length=24, null=True)
+    users = models.ManyToManyField(Profile, related_name='groups')
