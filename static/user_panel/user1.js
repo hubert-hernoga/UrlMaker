94420a4d4537a7bf646ac8cda742d5238d4f64d6
wezymord <hubert.hernoga@wp.pl>
2018-05-14 23:57:53 +0200

    Added new static files

---
+++ static/user_panel/user1.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+
+        function getCookie(c_name) {
+        if (document.cookie.length > 0)
+        {
+            c_start = document.cookie.indexOf(c_name + "=");
+            if (c_start != -1)
+            {
+                c_start = c_start + c_name.length + 1;
+                c_end = document.cookie.indexOf(";", c_start);
+                if (c_end == -1) c_end = document.cookie.length;
+                return unescape(document.cookie.substring(c_start,c_end));
+            }
+        }
+        return "";
+    }
+
+        $(function () {
+        $.ajaxSetup({
+            headers: { "X-CSRFToken": getCookie("csrftoken") }
+        });
+    });
+
+
+    // Removing user from list
+    $('.remove-user').click(function () {
+        var current_user = $('.current-user');
+        var user_id = this.id;
+
+        // Removing user from list
+        $.each( current_user, function(index, value) {
+            console.log(user_id)
+            if (user_id === value.id) {
+                value.remove();
+            }
+        });
+
+        $.ajax({
+            type: "DELETE",
+            url: "/",
+            data: {
+                "user_id": user_id
+            }
+        });
+    });
+    console.log('sdads')
+    // Removing group from list
+    $('.remove-group').click(function () {
+        var current_group = $('.current-group');
+        var group_id = this.id;
+
+        // Removing group from list
+        $.each( current_group, function(index, value) {
+            if (group_id === value.id) {
+                value.remove();
+            }
+        });
+
+        $.ajax({
+            type: "DELETE",
+            url: "/groups_list/",
+            data: {
+                "group_id": group_id
+            }
+        });
+    });
+});
