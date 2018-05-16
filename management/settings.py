435fcb3a92fb923dcfc6af96e991955b86ccb23c
wezymord <hubert.hernoga@wp.pl>
2018-05-16 20:32:34 +0200

    Fixed deploy

--- management/settings.py
+++ management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ')7!ibew*4+(&ezv^aqsmol!e8&jo#-ilh8)b45l2(wru%cd9s-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['user-management-.herokuapp.com', 'localhost', '0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['user-management-a.herokuapp.com', 'localhost', '0.0.0.0', '127.0.0.1']
 
 
 # Application definition
