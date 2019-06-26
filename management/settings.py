6a237778a5e35ec62f57136d675c1d7d0fa30b69
wezymord <hubert.hernoga@wp.pl>
2019-06-27 00:59:39 +0200

    dsadsasdfkk

--- management/settings.py
+++ management/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user_panel.apps.UserPanelConfig',
+    'user_panel.apps.UserPanelConfig'
 ]
 
 MIDDLEWARE = [
