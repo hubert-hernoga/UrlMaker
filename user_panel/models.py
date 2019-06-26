7b01886adf17d2819e1c9ef711d7ba8ac5751eee
wezymord <hubert.hernoga@wp.pl>
2019-06-26 18:06:01 +0200

    Migrated db

--- user_panel/models.py
+++ user_panel/models.py
@@ -4,8 +4,9 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 import datetime
 
+
 class Url(models.Model):
-    url = models.CharField(max_length=24, null=True)
+    result = models.CharField(max_length=24, null=True)
 
     def __str__(self):
-        return '{}'.format(self.url)
+        return '{}'.format(self.result)
